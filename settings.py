f332ca8d7c1f2df975305f74078a3790543d6177
ishtiaq11 <ishtiaqpias@gmail.com>
2019-11-06 13:11:06 +0600

    Add postgres db config to read from dotenv

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
 import os
-import dj-database-url
+
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,8 +12,20 @@ INSTALLED_APPS = (
 # settings for postgres database
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(
-    default="postgres://user:password@host:port/name", 
+    default="postgres://{user}:{password}@{host}:{port}/{name}".format(
+        user=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASSWORD"),
+        host=os.getenv("DATABASE_HOST"),
+        port=os.getenv("DATABASE_PORT"),
+        name=os.getenv("DATABASE_NAME")
+    ), 
     conn_max_age=600
 )
+if os.getenv("DATABASE_SCHEMA") is not None:
+    options = {}
+    options["options"] = "-c search_path={schema}".format(
+        schema=os.getenv("DATABASE_SCHEMA")
+    )
+    DATABASES["default"]["OPTIONS"] = options
 
 SECRET_KEY = 'REPLACE_ME'
